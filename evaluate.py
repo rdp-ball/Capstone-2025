3e89031b1154805be54763fa5d37453265e4b542
Rolwin Pinto <142580084+rdp-ball@users.noreply.github.com>
2024-11-12 19:53:14 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,5 @@
+ 
+import gymnasium as gym
 import numpy as np
 import os, sys
 from stable_baselines3 import PPO
@@ -9,18 +11,20 @@ from stable_baselines3.common.evaluation import evaluate_policy
 import pathlib
 
 def main():
-    gui = True
+    gui = False
     # defining model path
-    result_name = ""
-    timestep_mil = 14
-    save_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "Models", result_name, f"rl_model_{int(timestep_mil * 10 ** 6)}_steps.zip")
-
-    model = PPO.load(save_path)
-    env = SubprocVecEnv([lambda: SumoEnv(gui=gui)])
+    model_folder = "91_PPO_100"
+    save_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "Models", model_folder, f"{model_folder}.zip")
+   # save_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "Models", result_name, f"rl_model_{int(timestep_mil * 10 ** 6)}_steps")
+    #save_path = "/Users/rolwinpinto/onramp_simulation/Models/rl_model_14000000_steps.zip"  # Update this path if necessary
+    # save_path="/Users/rolwinpinto/onramp_simulation/Models/rl_model_14000000_steps"  # Check if this file exists
+    # Ensure the file is named correctly and has the .zip extension if needed
+    model = PPO.load(save_path)  # This line will raise an error if the file does not exist
+    env = DummyVecEnv([lambda: SumoEnv(gui=gui)])
     reward = 0
     obs = env.reset()
     time = 0
-    for _ in range(10000000):
+    for _ in range(10000):
         action, _ = model.predict(obs, deterministic=True)
         obs, rewards, done, info = env.step(action)
     
